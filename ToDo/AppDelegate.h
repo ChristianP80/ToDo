35d82fdf35ad6690e1aa144a6495dc98b28ccbf6
christianpersson <chrissp_@hotmail.com>
2018-01-27 21:16:24 +0100

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Christian Persson on 2018-01-27.
+//  Copyright © 2018 Christian Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
